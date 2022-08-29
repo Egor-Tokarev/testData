f9986f02af32e09ed40f2c6712db7e2877c6f3f0
Egor-Tokarev <egortokarev59@gmail.com>
2022-08-29 16:49:21 +0300

    add readme

--- src/test/java/com/demoqa/pages/components/CalendarComponent.java
+++ src/test/java/com/demoqa/pages/components/CalendarComponent.java
@@ -13,10 +13,3 @@ public class CalendarComponent {
 
 }
 
-
-
-        app_settings=eyJhZ3JlZW1lbnRzIjoiMTExIiwicHJvbW8iOiIxMTEiLCJ2a2NfYmVoYXZpb3IiOiIiLCJ2a2NfYXV0aF9hY3Rpb24iOiIiLCJ2a2NfYnJhbmQiOiIiLCJ2a2NfZGlzcGxheV9tb2RlIjoiIn0%3D
-
-        app_settings=eyJhZ3JlZW1lbnRzIjoiMTExIiwicHJvbW8iOiIxMTEiLCJ2a2NfYmVoYXZpb3IiOiIiLCJ2a2NfYXV0aF9hY3Rpb24iOiIiLCJ2a2NfYnJhbmQiOiIiLCJ2a2NfZGlzcGxheV9tb2RlIjoiIn0%3D
-
-https://id.vk.com/auth?app_id=7303035&v=1.54.0&redirect_uri=https%3A%2F%2Fdemo.vksak.com&uuid=kvXEuEgVjNvaqbNcnjLKy&scheme=bright_light&app_settings=eyJhZ3JlZW1lbnRzIjoiMTExIiwicHJvbW8iOiIxMTEiLCJ2a2NfYmVoYXZpb3IiOiIiLCJ2a2NfYXV0aF9hY3Rpb24iOiIiLCJ2a2NfYnJhbmQiOiIiLCJ2a2NfZGlzcGxheV9tb2RlIjoiIn0%3D
